0b802f31dbb9afb47c6f95167e4e5ca2660882b3
Marco Antonio Zanandrea <marco13zanandrea@gmail.com>
2026-03-08 16:12:11 -0300

    Dev - Separando Header e Criação de Arquivos PHP

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <!-- CABEÇALHO -->
+    <header class="header">
+        <div class="top_promo" id="topPromo">
+            <a class="promo_text">
+                Pague com <strong>PIX</strong> ou <strong>Cartão</strong> em até <strong>10x</strong> sem juros
+            </a>
+        </div>
+
+        <div class="header_container">
+            <a class="logo">
+                <div>
+                    <img src="img_padrao/mascote_cobra.png" alt="Logo Eletronica Cobra" width="60px" height="60px">
+                </div>
+            </a>
+
+            <div class="barra_pesquisa">
+                <div class="box_pesquisa">
+                    <img src="icons/lupa.png" alt="Ícone de lupa" class="icon_pesquisa" width="20" height="20">
+                    <input type="text" placeholder="Buscar guitarras, baterias, teclados...">
+                </div>
+            </div>
+
+            <div class="menu">
+                <nav class="nav"> <!-- depois linkar com as classes respectivas-->
+                    <a href="#">Início</a>
+                    <a href="#produtos">Produtos</a>
+                    <a href="#sobre">Sobre</a>
+                    <a href="#contato">Contato</a>
+                </nav>
+
+                <button class="btn_header" type="button">
+                    <img src="icons/carrinho.png" alt="Ícone do carrinho" width="20" height="20">
+                    Carrinho
+                </button>
+
+                <button class="btn_header" type="button">
+                    <img src="icons/login.png" alt="Ícone do login" width="20" height="20">
+                    <!-- Login -->
+                </button>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,49 +10,9 @@
     <link rel="icon" type="image/png" href="img_padrao/mascote_cobra.png">
 </head>
 
+
 <body>
-    <!-- CABEÇALHO -->
-    <header class="header">
-        <div class="top_promo" id="topPromo">
-            <a class="promo_text">
-                Pague com <strong>PIX</strong> ou <strong>Cartão</strong> em até <strong>10x</strong> sem juros
-            </a>
-        </div>
-
-        <div class="header_container">
-            <a class="logo">
-                <div>
-                    <img src="img_padrao/mascote_cobra.png" alt="Logo Eletronica Cobra" width="60px" height="60px">
-                </div>
-            </a>
-
-            <div class="barra_pesquisa">
-                <div class="box_pesquisa">
-                    <img src="icons/lupa.png" alt="Ícone de lupa" class="icon_pesquisa" width="20" height="20">
-                    <input type="text" placeholder="Buscar guitarras, baterias, teclados...">
-                </div>
-            </div>
-
-            <div class="menu">
-                <nav class="nav"> <!-- depois linkar com as classes respectivas-->
-                    <a href="#">Início</a>
-                    <a href="#produtos">Produtos</a>
-                    <a href="#sobre">Sobre</a>
-                    <a href="#contato">Contato</a>
-                </nav>
-
-                <button class="btn_header" type="button">
-                    <img src="icons/carrinho.png" alt="Ícone do carrinho" width="20" height="20">
-                    Carrinho
-                </button>
-
-                <button class="btn_header" type="button">
-                    <img src="icons/login.png" alt="Ícone do login" width="20" height="20">
-                    <!-- Login -->
-                </button>
-            </div>
-        </div>
-    </header>
+    <?php include 'header.php'; ?>
 
     <!-- IMAGEM FUNDO DA LOGO -->
     <section class="banner">
@@ -84,7 +44,6 @@
             <button class="categorias_seta categorias_seta_dir" type="button">›</button>
         </div>
     </section>
-
 
     <!-- PRODUTOS -->
     <!-- depois adicionar um for com php para criar automatico os cards e puxar do bd os produtos -->
